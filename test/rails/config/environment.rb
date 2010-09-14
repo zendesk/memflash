5edbdb1d41bf781fdc43bf4fd1d5d0da73fbc3ae
Mick Staugaard <mick@staugaard.com>
2010-09-14 14:14:48 -0700

    update test environment to use shoulda from rubygems.org

--- test/rails/config/environment.rb
+++ test/rails/config/environment.rb
@@ -5,5 +5,5 @@ Rails::Initializer.run do |config|
   config.frameworks -= [ :active_record ]
   config.action_controller.session = { :key => "_memflash_session", :secret => "ac0dcc5b4d9eef497e671126a845c3c5" }
   
-  config.gem 'thoughtbot-shoulda', :lib => 'shoulda/rails', :source => 'http://gems.github.com'
+  config.gem 'shoulda', :lib => 'shoulda/rails'
 end
