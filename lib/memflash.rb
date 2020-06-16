4beb98327bc9189dcb81b82c6025f5a6cfcc197f
Ryan Davis <ryand-ruby@zenspider.com>
2020-06-16 11:00:16 -0700

    Updated to work without alias_method_chain to be compatible with rails 5.
    Just using a regular Module prepend.
    
    Also removed references to rails 3.

--- lib/memflash.rb
+++ lib/memflash.rb
@@ -5,14 +5,7 @@ module Memflash
   self.threshold = 300 # Messages longer than this will be stored in Rails.cache
 
   module CachingLayer
-    def self.included(base)
-      base.class_eval do
-        alias_method_chain :[]=, :caching
-        alias_method_chain :[], :caching
-      end
-    end
-
-    define_method "[]_with_caching=" do |key, value|
+    def []=(key, value)
       value_for_hash = value
 
       if value.kind_of?(String) && value.length >= Memflash.threshold
@@ -20,12 +13,16 @@ module Memflash
         Rails.cache.write(value_for_hash, value)
       end
 
-      send("[]_without_caching=", key, value_for_hash)
+      super(key, value_for_hash)
     end
 
-    define_method "[]_with_caching" do |key|
-      value_in_hash = send("[]_without_caching", key)
-      memflashed?(key, value_in_hash) ? Rails.cache.read(value_in_hash) : value_in_hash
+    def [](key)
+      value_in_hash = super
+      if memflashed?(key, value_in_hash)
+        Rails.cache.read(value_in_hash)
+      else
+        value_in_hash
+      end
     end
 
     private
@@ -35,17 +32,10 @@ module Memflash
     end
 
     def memflashed?(key, value)
-      !!(value =~ /^Memflash-#{key}/)
+      /^Memflash-#{key}/.match? value
     end
   end
 end
 
-require 'action_pack/version'
-base = if ActionPack::VERSION::MAJOR >= 3
-  require 'action_dispatch'
-  ActionDispatch::Flash::FlashHash
-else
-  require 'action_controller'
-  ActionController::Flash::FlashHash
-end
-base.send :include, Memflash::CachingLayer
+require 'action_dispatch'
+ActionDispatch::Flash::FlashHash.prepend Memflash::CachingLayer
